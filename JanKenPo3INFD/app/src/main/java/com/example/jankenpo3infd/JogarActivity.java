c9634d035a0f066a7b7eaacb4705a310e564bb47
pQuintans <caixote.dom@gmail.com>
2022-03-24 10:44:50 -0300

    Início do Projeto de JanKenPo - Feito em sala

---
+++ JanKenPo3INFD/app/src/main/java/com/example/jankenpo3infd/JogarActivity.java
@@ -0,0 +1,14 @@
+package com.example.jankenpo3infd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JogarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogar);
+    }
+}
